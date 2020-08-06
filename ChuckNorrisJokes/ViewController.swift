435a6b4f812915ee1574c3a2cad18ea94e4c639a
yegorheiz <yegorheiz@gmail.com>
2020-08-07 00:30:19 +0300

    Initial Commit

---
+++ ChuckNorrisJokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Yegor Heiz on 07.08.2020.
+//  Copyright © 2020 Yegor Heiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
